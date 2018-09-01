b37f70e8b032f84c5bee04767f4e55c4c8c52554
15914145294 <422079695@qq.com>
2018-09-01 11:18:17 +0800

    add main.py

--- main.py
+++ main.py
@@ -7,3 +7,44 @@
    date：          2018/9/1 0001
 -------------------------------------------------
 """
+
+import os
+import unittest
+
+from configs.config import *
+from utils.HTMLTestRunner_PY3 import HTMLTestRunner
+from utils.constants import time_tag
+from logs.logUtil import logger
+from utils.mail import Email
+from utils.util import UtilMethod
+
+if __name__ == '__main__':
+	logger.info("-" * 40 + "测试开始" + "-" * 40)
+	report = REPORT_PATH + '\\report_%s.html' % time_tag
+	suite = unittest.TestSuite()
+	# case_home = unittest.TestLoader().loadTestsFromTestCase(CaseHome)
+	# case_menu = unittest.TestLoader().loadTestsFromTestCase(CaseMenu)
+	# tests = [case_home, case_menu]
+	discover = unittest.defaultTestLoader.discover(CASE_PATH, pattern='test_*.py')
+	suite.addTests(discover)
+	with open(report, 'wb+') as f:
+		runner = HTMLTestRunner(stream=f, verbosity=2, title='自动化接口测试',
+		                        description=''
+		                        )
+		runner.run(suite)
+	logger.info("-" * 40 + "测试结束" + "-" * 40)
+	logger.info("-" * 40 + "发送邮件" + "-" * 40)
+	mail_cf = Config().get("mail")
+	log_path = os.path.join(LOG_PATH, "%stest.log" % time_tag)
+	accessory_list = [log_path, report]
+
+	report_html = UtilMethod.read_file(report)
+	# e = Email(title='接口测试报告',
+	#           receiver=mail_cf.get("EMAIL_RECEIVER"),
+	#           server=mail_cf.get("EMAIL_SERVER"),
+	#           sender=mail_cf.get("EMAIL_SENDER"),
+	#           password=mail_cf.get("EMAIL_PASSWORD"),
+	#           message=report_html,
+	#           path=accessory_list
+	#           )
+	# e.send()
